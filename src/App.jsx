e2ea2419a7d56ccd41783b54db172951c70403a0
Pascal-Juma <pascaljuma2002@gmail.com>
2025-03-17 16:39:17 +0300

    Fix: Footer Unresponsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,6 @@ function App() {
   return (
     <>
       <Home />
-      <div className="footer-section">
-      <p className="footer-details">
-        &copy; 2025 Pascal Juma. All Rights Reserved. Powered by Creative  <span className="footer-branding">  Vortex.</span>
-      </p>
-    </div>
     </>
   )
 }
